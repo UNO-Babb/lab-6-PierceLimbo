ea894a3c249f5d2c27a7370104cb5e65aa31cece
Pierce Limbo <plimbo@unomaha.edu>
2025-10-05 11:09:21 -0500

    Update WordCount.py

--- WordCount.py
+++ WordCount.py
@@ -1,13 +1,28 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name:Pierce Limbo
+#Date:10/5/2025
+#Assignment: Lab6
 
 def main():
+  
   textFile = open("gettysberg.txt", 'r')
+
+  with textFile as TextFile:
+    line_count = 0
+    word_count = 0
+    character_count = 0
   
   for line in textFile:
     print(line)
+    line_count += 1
+    words = line.split()
+    word_count += len(words)
+    character_count += len(line)
+
+print(f"Lines: {line_count}")
+print(f"Words: {word_count}")
+print(f"Characters: {character_count}")
+
   
 
 if __name__ == '__main__':
